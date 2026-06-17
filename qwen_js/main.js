1335fb87f95629460ae783d8a4d4dc651175f378
Neantdinf0 <lucie.ratie@insa-cvl.fr>
2026-06-17 17:44:06 +0200

    commit debut de vectorization et de langchain

--- qwen_js/main.js
+++ qwen_js/main.js
@@ -1,3 +1,7 @@
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { OllamaLLM } from "@langchain/ollama";
+import dotenv from "dotenv";
+
 /**
  * Vectorise et stocke une liste de fichiers dans la base de données RAG.
  *
@@ -43,4 +47,28 @@ function invoke(prompt) {
  */
 function get_discussion() {
     // retourner l'ensemble [{role:"user",content:"je ne comprend pas..."}]
+}
+
+//Initialisation dotenv
+dotenv.config();
+
+export function getModel(isOnline, temperature) {
+    let model;
+    if (is_online) {
+        console.log("🤖 Initialisation de Gemini (Online)...")
+        model = new ChatGoogleGenerativeAI({
+            modelName: "gemini-2.5-flash",
+            temperature: temperature,
+        });
+    }
+    else {
+        console.log("🦙 Initialisation de Ollama/Llama3 (Local)...")
+        model = new OllamaLLM({
+            modelName:"qwen-no-think",
+            temperature: temperature,
+            maxRetries:3,
+            baseUrl:"http://localhost:11434"
+        });
+    }
+    return model;
 }
