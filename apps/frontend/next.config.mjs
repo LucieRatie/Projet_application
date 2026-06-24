c6fa4b06d242322cc22cb75d6bdb63101d17939f
Théophile BENZARIA <theophile.benzaria@gmail.com>
2026-06-24 23:20:50 +0200

    Update next.config.mjs

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,5 +1,5 @@
 /*const { withAui } = await import("@assistant-ui/next");*/
-import { withAui } from "@assistant-ui/next";
+//import { withAui } from "@assistant-ui/next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@assistant-ui/react", "@assistant-ui/react-ai-sdk"],
