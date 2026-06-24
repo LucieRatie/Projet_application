d0fba21a6676722a24def1520650f890e316ea5a
Théophile BENZARIA <theophile.benzaria@gmail.com>
2026-06-24 22:37:24 +0200

    retour en arrière

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,4 +1,5 @@
 /*const { withAui } = await import("@assistant-ui/next");*/
+import { withAui } from "@assistant-ui/next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@assistant-ui/react", "@assistant-ui/react-ai-sdk"],
@@ -19,4 +20,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withAui(nextConfig);
